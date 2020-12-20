59f7d8c278f702fc08cde11254d489b7165f4dd2
Ben Werdmuller <ben@benwerd.com>
2020-12-19 16:24:16 -0800

    Support for new-style IDs

--- Main.php
+++ Main.php
@@ -8,9 +8,9 @@ namespace IdnoPlugins\Text {
         function registerPages()
         {
             \Idno\Core\Idno::site()->routes()->addRoute('/entry/edit/?', '\IdnoPlugins\Text\Pages\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/entry/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/entry/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Delete');
-            \Idno\Core\Idno::site()->routes()->addRoute('/entry/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
+            \Idno\Core\Idno::site()->routes()->addRoute('/entry/edit/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Text\Pages\Edit');
+            \Idno\Core\Idno::site()->routes()->addRoute('/entry/delete/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Text\Pages\Delete');
+            \Idno\Core\Idno::site()->routes()->addRoute('/entry/([A-Za-z0-9\-]+)/.*', '\Idno\Pages\Entity\View');
         }
 
         function registerTranslations()
